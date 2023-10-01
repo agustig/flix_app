6c292918f26106d8562c3977afe8aba43b19dc03
agustig <agusti.g@icloud.com>
2023-10-01 14:49:49 +0900

    add upload profile picture usecase

---
+++ lib/domain/usecases/upload_profile_picture/upload_profile_picture_params.dart
@@ -0,0 +1,8 @@
+part of 'upload_profile_picture.dart';
+
+class UploadProfilePictureParams {
+  final File imageFile;
+  final User user;
+
+  UploadProfilePictureParams({required this.imageFile, required this.user});
+}
