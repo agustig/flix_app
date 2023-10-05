3c30b4062180ce0c3d64b682721b980a9671c386
agustig <agusti.g@icloud.com>
2023-10-06 00:09:34 +0900

    add main page

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,6 +1,8 @@
 import 'package:flix_app/presentation/extensions/build_context_extension.dart';
 import 'package:flix_app/presentation/providers/router/router_provider.dart';
 import 'package:flix_app/presentation/providers/user_data/user_data_provider.dart';
+import 'package:flix_app/presentation/widgets/bottom_nav_bar.dart';
+import 'package:flix_app/presentation/widgets/bottom_nav_bar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,6 +14,8 @@ class MainPage extends ConsumerStatefulWidget {
 }
 
 class _MainPageState extends ConsumerState<MainPage> {
+  final pageController = PageController();
+  int selectedPage = 0;
   @override
   Widget build(BuildContext context) {
     ref.listen(userDataProvider, (previous, next) {
@@ -22,25 +26,54 @@ class _MainPageState extends ConsumerState<MainPage> {
       }
     });
     return Scaffold(
-      appBar: AppBar(title: const Text('Main Page')),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              ref.watch(userDataProvider).when(
-                    data: (data) => data.toString(),
-                    error: (error, stackTrace) => '',
-                    loading: () => 'Loading',
-                  ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(userDataProvider.notifier).logout();
-              },
-              child: const Text('Logout'),
-            )
-          ],
-        ),
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageController,
+            onPageChanged: (value) => setState(() {
+              selectedPage = value;
+            }),
+            children: const [
+              Center(child: Text('Movie Page')),
+              Center(child: Text('Ticket Page')),
+              Center(child: Text('Profile Page')),
+            ],
+          ),
+          BottomNavBar(
+            items: [
+              BottomNavBarItem(
+                index: 0,
+                isSelected: selectedPage == 0,
+                title: 'Home',
+                image: 'assets/movie.png',
+                selectedImage: 'assets/movie-selected.png',
+              ),
+              BottomNavBarItem(
+                index: 1,
+                isSelected: selectedPage == 1,
+                title: 'Ticket',
+                image: 'assets/ticket.png',
+                selectedImage: 'assets/ticket-selected.png',
+              ),
+              BottomNavBarItem(
+                index: 2,
+                isSelected: selectedPage == 2,
+                title: 'Profile',
+                image: 'assets/profile.png',
+                selectedImage: 'assets/profile-selected.png',
+              ),
+            ],
+            onTap: (index) {
+              selectedPage = index;
+              pageController.animateToPage(
+                selectedPage,
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeInOut,
+              );
+            },
+            selectedIndex: 0,
+          )
+        ],
       ),
     );
   }
